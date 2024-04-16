3179f70c96e700e23d4eecf2048941b527e5499c
sivadeepkumar <sivadeepkumar3@gmail.com>
2024-04-16 16:08:46 +0530

    changes to llama2 model

--- ai_models/meta/llama_2/app.py
+++ ai_models/meta/llama_2/app.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify, Blueprint
+from flask import request,Flask, jsonify, Blueprint
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain_community.llms import CTransformers
@@ -7,18 +7,20 @@ import logging
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-logging.basicConfig(filename='../logs/llama2_model.log', level=logging.INFO)
+app = Flask(__name__)
+logging.basicConfig(filename='../../../logs/llama2_model.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 # load documents
-documents = SimpleDirectoryReader("./sample_data/pdf_samples/").load_data()
+documents = SimpleDirectoryReader("../../../sample_data/pdf_samples").load_data()
 
 # Initialize HuggingFace Embeddings
 embed_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 # embed_model = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5") #-- We can also use this model
 
 # Initialize LLama2 model (load it only once)
-llm = CTransformers(model='llama-2-7b-chat.ggmlv3.q8_0.bin',
+llm = CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
                     model_type='llama',
                     config={'max_new_tokens': 256,
                             'temperature': 0.01, 'context_length': 1800},
@@ -34,9 +36,9 @@ index = VectorStoreIndex.from_documents(documents,service_context=Settings)
 # Initialize Query Engine
 query_engine = index.as_query_engine()
 
-app = Blueprint('llama_openai_model', __name__)
+# app = Blueprint('llama_openai_model', __name__)
 
-@app.route('/query', methods=['POST'])
+@app.route('/llama2/text-generation', methods=['POST'])
 def generate_response():
     """
     Generates a response to a user query using the LLama2 model.
@@ -69,3 +71,7 @@ def generate_response():
     except Exception as e:
         logging.error(f"An error occurred: {e}")
         return jsonify({"error": "An error occurred"}), 500
+
+
+if __name__ == '__main__':
+    app.run(port= 8000)
